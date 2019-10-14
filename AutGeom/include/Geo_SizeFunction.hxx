b2a7a430e34f541849aba0de44915aac686137f3
amir5200fx <aasoleimani86@gmail.com>
2019-10-14 08:20:50 -0500

    Adding AutMeshAft

---
+++ AutGeom/include/Geo_SizeFunction.hxx
@@ -0,0 +1,50 @@
+#pragma once
+#ifndef _Geo_SizeFunction_Header
+#define _Geo_SizeFunction_Header
+
+#include <Standard_TypeDef.hxx>
+#include <Global_Done.hxx>
+#include <Global_Named.hxx>
+#include <Global_Indexed.hxx>
+
+namespace AutLib
+{
+
+	template<class Point>
+	class Geo_SizeFunction
+		: public Global_Indexed
+		, public Global_Named
+		, public Global_Done
+	{
+
+		/*Private Data*/
+
+	protected:
+
+		Geo_SizeFunction()
+		{}
+
+		Geo_SizeFunction
+		(
+			const Standard_Integer theIndex, 
+			const word& theName
+		)
+			: Global_Indexed(theIndex)
+			, Global_Named(theName)
+		{}
+
+	public:
+
+		
+		//- virtual functions and operators
+
+		virtual Standard_Real Value(const Point& theCoord) const = 0;
+
+		virtual void Perform()
+		{
+			Change_IsDone() = Standard_True;
+		}
+	};
+}
+
+#endif // !_Geo_SizeFunction_Header
