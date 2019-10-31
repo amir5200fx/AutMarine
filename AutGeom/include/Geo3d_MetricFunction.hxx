47da3164ce2e9892c63779f685f7ce43534292af
amir5200fx <aasoleimani86@gmail.com>
2019-10-31 02:58:14 -0500

    adding Boundary entities for iso and aniso two-dimensional mesh generation

---
+++ AutGeom/include/Geo3d_MetricFunction.hxx
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef _Geo3d_MetricFunction_Header
+#define _Geo3d_MetricFunction_Header
+
+#include <Geo_MetricFunction.hxx>
+#include <Pnt3d.hxx>
+
+namespace AutLib
+{
+
+	typedef Geo_MetricFunction<Pnt3d> Geo3d_MetricFunction;
+}
+
+#endif // !_Geo3d_MetricFunction_Header
