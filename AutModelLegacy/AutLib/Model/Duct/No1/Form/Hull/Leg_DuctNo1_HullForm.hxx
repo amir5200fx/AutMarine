720278bb22e6a78f901ee60e8e6107eaa9b58113
amir5200fx <aasoleimani86@gmail.com>
2019-11-15 11:18:48 -0600

    adding legDuct project

---
+++ AutModelLegacy/AutLib/Model/Duct/No1/Form/Hull/Leg_DuctNo1_HullForm.hxx
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef _Leg_DuctNo1_HullForm_Header
+#define _Leg_DuctNo1_HullForm_Header
+
+#include <Global_AccessMethod.hxx>
+#include <Leg_PropNo1_Form.hxx>
+#include <Leg_Model_Parameter.hxx>
+
+#include <memory>
+
+namespace AutLib
+{
+
+	class Leg_DuctNo1_HullForm
+	{
+
+		/*Private Data*/
+
+	protected:
+
+		void Defaults();
+
+	public:
+
+		Leg_DuctNo1_HullForm();
+
+
+	};
+}
+
+#endif // !_Leg_DuctNo1_HullForm_Header
